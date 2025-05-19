0cec0300df906159deaaf833d89442cd6c73b387
Rafael Caslu Henry <rafaelhenrycassiano23@gmail.com>
2025-05-19 00:06:23 -0300

    feat: created homePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset(
+              'assets/pagelogo.png',
+              width: double.infinity,
+              height: 50,
+              fit: BoxFit.contain,
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Text(
+                'Promoções do dia',
+                style: GoogleFonts.poppins(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
