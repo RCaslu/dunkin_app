2c248b16d9d18d9aaa43b33e70b9a5c169cfacff
Rafael Caslu Henry <rafaelhenrycassiano23@gmail.com>
2025-05-25 18:12:57 -0300

    fix: fixed the cupons page

--- lib/pages/cupons_page.dart
+++ lib/pages/cupons_page.dart
@@ -1,42 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CuponsPage extends StatefulWidget {
-  const CuponsPage({Key? key}) : super(key: key);
+  const CuponsPage({super.key});
 
   @override
   State<CuponsPage> createState() => _CuponsPageState();
 }
 
 class _CuponsPageState extends State<CuponsPage> {
-  int currentIndex = 0;
-
   @override
   Widget build(BuildContext context) {
-    final cupons = [
-      {'title': '10% OFF', 'description': 'Get 10% off on your next order!'},
-      {'title': 'Free Donut', 'description': 'Buy 2 donuts, get 1 free!'},
-      {'title': '20% OFF', 'description': '20% off for orders above \$20.'},
-    ];
-
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: const Text('Cupons'),
-        backgroundColor: Colors.white,
-      ),
-      body: ListView.builder(
-        itemCount: cupons.length,
-        itemBuilder: (context, index) {
-          final cupon = cupons[index];
-          return Card(
-            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: ListTile(
-              leading: const Icon(Icons.card_giftcard, color: Colors.orange),
-              title: Text(cupon['title']!),
-              subtitle: Text(cupon['description']!),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Text(
+              "Cupons",
+              style: GoogleFonts.poppins(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          );
-        },
+          ],
+        ),
       ),
     );
   }
