67ca8b6b18d32d6ffa41f36a7c389afc688c2fad
Rafael Caslu Henry <rafaelhenrycassiano23@gmail.com>
2025-05-29 23:09:17 -0300

    feat: added product section widget
    Created a new `productsection` widget in `product_sections.dart`.
    The widget includes a styled column with a title and a horizontally scrollable row displaying multiple `cuponsProductCard` components.

---
+++ lib/sections/product_sections.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../components/cards/cupons_product_card.dart';
+
+Widget productsection(String columTitle) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      const SizedBox(height: 20),
+      Padding(
+        padding: const EdgeInsets.only(left: 10),
+        child: Text(
+          columTitle.toUpperCase(),
+          style: GoogleFonts.poppins(
+            fontSize: 20,
+            fontWeight: FontWeight.w900,
+            color: Color(0xFFF20C90),
+          ),
+        ),
+      ),
+      SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          children: [
+            cuponsProductCard(),
+            cuponsProductCard(),
+            cuponsProductCard(),
+            cuponsProductCard(),
+          ],
+        ),
+      ),
+    ],
+  );
+}
