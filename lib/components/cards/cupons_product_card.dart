f254790021ca947b56937909f2948f100995adc7
Rafael "Caslu" Henry Cassiano Luiz <rafaelhenrycassiano23@gmail.com>
2025-05-29 21:10:32 -0300

    feat: added the cuponsProductCard

---
+++ lib/components/cards/cupons_product_card.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Widget cuponsProductCard() {
+  return Padding(
+    padding: const EdgeInsets.all(4.0),
+    child: Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Image.asset('assets/i1.png'),
+            Text(
+              "Ninho com avelã".toUpperCase(),
+              style: GoogleFonts.poppins(
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 5),
+            Text(
+              "50% OFF",
+              style: GoogleFonts.poppins(
+                fontSize: 12,
+                color: Color(0xFFE38B3D),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
