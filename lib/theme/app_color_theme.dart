45794273f2fe264c7b5096fd601c5ccd3a8eec80
Rafael Caslu Henry <rafaelhenrycassiano23@gmail.com>
2025-06-03 20:30:08 -0300

    feat: added typography and color files on project

---
+++ lib/theme/app_color_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  // Cores
+  static const Color orangeDunkin = Color(0xFFFF6E0C);
+  static const Color pinkDunkin = Color(0xFFFF6E0C);
+  static const Color backgroundColor = Colors.white;
+  static const Color dividerColor = Colors.grey;
+}
---
+++ lib/theme/app_typography_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTheme {
+  // Tipografia
+  static final TextStyle titleStyle = GoogleFonts.poppins(
+    fontSize: 24,
+    fontWeight: FontWeight.w900,
+  );
+}
