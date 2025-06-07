8391557e7245c0bb92e867b1994467b613493ef0
Rafael "Caslu" Henry Cassiano Luiz <rafaelhenrycassiano23@gmail.com>
2025-06-06 22:51:58 -0300

    refactor: simplify constructor syntax for CustomNavBar and MainPage

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -5,7 +5,7 @@ import 'package:dunkin_app/pages/profile_page.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatefulWidget {
-  const MainPage({Key? key}) : super(key: key);
+  const MainPage({super.key});
 
   @override
   _MainPageState createState() => _MainPageState();
