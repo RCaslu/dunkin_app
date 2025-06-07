4f287d8848709bc104d2cd95ddfdabd8242b4e46
Rafael "Caslu" Henry Cassiano Luiz <rafaelhenrycassiano23@gmail.com>
2025-06-06 22:51:27 -0300

    feat: enhance ProfilePage layout with user details and navigation options

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 // Adjust the path if needed
 
 class ProfilePage extends StatefulWidget {
-  const ProfilePage({Key? key}) : super(key: key);
+  const ProfilePage({super.key});
 
   @override
   State<ProfilePage> createState() => _ProfilePageState();
@@ -15,8 +16,84 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: const Center(
-        child: Text('This is the Profile Page', style: TextStyle(fontSize: 24)),
+      body: Column(
+        children: [
+          Center(
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 80,
+                  backgroundImage: AssetImage('assets/pfpc.png'),
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  'Rafael Henry',
+                  style: GoogleFonts.poppins(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  'rafael.henry@email.com',
+                  style: GoogleFonts.poppins(
+                    fontSize: 16,
+                    color: Colors.grey[700],
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  'CPF: ***.***.789-00',
+                  style: GoogleFonts.poppins(
+                    fontSize: 16,
+                    color: Colors.grey[700],
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  'Data de nascimento: **/**/1990',
+                  style: GoogleFonts.poppins(
+                    fontSize: 16,
+                    color: Colors.grey[700],
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Divider(thickness: 1, color: Colors.grey[300]),
+                const SizedBox(height: 20),
+                ListTile(
+                  leading: Icon(Icons.local_offer, color: Color(0xFFFF6E0C)),
+                  title: Text(
+                    'Meus Cupons',
+                    style: GoogleFonts.poppins(fontSize: 18),
+                  ),
+                  trailing: Icon(Icons.arrow_forward_ios, size: 16),
+                  onTap: () {
+                    // Navegar para a página de cupons
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.shopping_bag, color: Color(0xFFE38B3D)),
+                  title: Text(
+                    'Meus Pedidos',
+                    style: GoogleFonts.poppins(fontSize: 18),
+                  ),
+                  trailing: Icon(Icons.arrow_forward_ios, size: 16),
+                  onTap: () {
+                    // Navegar para a página de pedidos
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.logout, color: Colors.red),
+                  title: Text('Sair', style: GoogleFonts.poppins(fontSize: 18)),
+                  onTap: () {
+                    // Implementar logout
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
